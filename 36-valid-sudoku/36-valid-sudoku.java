10d55824b25f872b161f99c4c2140766aba93e6b
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-01-04 16:37:35 +0530

    Time: 26 ms (5.80%), Space: 45.6 MB (5.12%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -1,52 +1,18 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        
-        for(int i=0; i<board.length; i++) 
-            if(!isRowValid(board[i]))
-                return false;
-        
-        for(int j=0; j<board[0].length; j++)
-            if(!isColumnValid(board, j))
-                return false;
-        
-        for(int i=0; i<board.length; i += 3)
-            for(int j=0; j<board[0].length; j += 3) 
-                if(!isGridValid(board, i, j))
-                    return false;
-        
-        return true;
-    }
-    
-    public boolean isRowValid(char[] rows) {
-        Set<Character> hs = new HashSet<Character>();
-        for(int i=0; i<rows.length; i++) {
-            char cur = rows[i];
-            if(cur != '.' && !hs.add(cur))
-                return false;
-        }
-        return true;
-    }
-    
-    public boolean isColumnValid(char[][] board, int col) {
-        Set<Character> hs = new HashSet<>();
-        for(int i=0; i<board[0].length; i++) {
-            char cur = board[i][col];
-            if(cur != '.' && !hs.add(cur))
-                return false;
-        }
-        return true;
-    }
-    
-    public boolean isGridValid(char[][] board, int row, int col) {
-        Set<Character> hs = new HashSet<>();
-        for(int i=row; i<row+3; i++) {
-            for(int j=col; j<col+3; j++) {
+        Set<String> hs = new HashSet<>();
+        for(int i=0; i<board.length; i++) {
+            for(int j=0; j<board[0].length; j++) {
                 char cur = board[i][j];
-                if(cur != '.' && !hs.add(cur))
-                    return false;
+                if(cur != '.') {
+                    if(!hs.add(cur + "found in row" +i) ||
+                       !hs.add(cur + "found in column" +j) ||
+                       !hs.add(cur + "found in grid" +i/3 +"-"+j/3)
+                      )
+                        return false;
+                }
             }
         }
         return true;
     }
-            
 }
