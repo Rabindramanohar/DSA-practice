7b82909f6313c1889e7fe570f3e932b21f169101
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-01-05 11:47:46 +0530

    Time: 5 ms (55.22%), Space: 42.5 MB (22.05%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,25 +1,14 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        if(ransomNote.length() == 0)
-            return false;
-        
-        String[] notes = ransomNote.split("");
-        String[] mags = magazine.split("");
-        Arrays.sort(notes);
-        Arrays.sort(mags);
-        int cnt = 0;
-        
-        for(String str: notes) {
-            if(cnt == mags.length)
-                return false;
-            while(!mags[cnt].equals(str)) {
-                if(cnt == mags.length-1)
-                    return false;
-                if(!mags[cnt].equals(str))
-                    cnt++;
-            }
-            cnt++;
+        int[] count = new int[26];
+        for(int i=0; i<magazine.length(); i++) {
+            count[magazine.charAt(i)-'a']++;
         }
-      return true;  
+        
+        for(int i=0; i<ransomNote.length(); i++) {
+            if(--count[ransomNote.charAt(i)-'a'] < 0)
+                return false;
+        }
+        return true;
     }
 }
