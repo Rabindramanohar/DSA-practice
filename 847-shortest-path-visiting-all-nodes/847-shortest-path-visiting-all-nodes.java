a61f9b3d873c01d01dc8691ea1e53e600619d706
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-02-27 03:06:54 +0530

    Time: 7 ms (100.00%), Space: 42.3 MB (84.01%) - LeetHub

--- 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.java
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.java
@@ -1,33 +1,36 @@
 class Solution {
-    private int[][] cache;
-    private int endingMask;
     public int shortestPathLength(int[][] graph) {
         int n = graph.length;
-        endingMask = (1<<n)-1;
-        cache = new int[n+1][endingMask+1];
+        if(n == 1) return 0;
+        int endingMask = (1<<n)-1;
+        boolean[][] seen = new boolean[n][endingMask];
+        int steps = 0;
+        ArrayList<int[]> q = new ArrayList<>();
         
-        int best = Integer.MAX_VALUE;
         for(int i=0; i<n; i++) {
-            best = Math.min(best, dp(i, endingMask, graph));
+            q.add(new int[]{i, 1<<i});
+            seen[i][1<<i] = true;
         }
-        return best;
-    }
-    
-    public int dp(int i, int mask, int[][] graph) {
-        if(cache[i][mask] != 0) 
-            return cache[i][mask];
-        if((mask & (mask-1)) == 0)
-            return 0;
         
-        cache[i][mask] = Integer.MAX_VALUE - 1;
-        for(int ng : graph[i]) {
-            if((mask & (1 << ng)) != 0) {
-                int alVis = dp(ng, mask, graph);
-                int notAlVis = dp(ng, mask ^ (1 << i), graph);
-                int betterOpt = Math.min(alVis, notAlVis);
-                cache[i][mask] = Math.min(cache[i][mask], 1 + betterOpt);
+        while(!q.isEmpty()) {
+            ArrayList<int[]> nextQ = new ArrayList<>();
+            for(int i=0; i<q.size(); i++) {
+                int[] currPair = q.get(i);
+                int node = currPair[0];
+                int mask = currPair[1];
+                for(int ng : graph[node]) {
+                    int nextMask = mask | (1<<ng);
+                    if(nextMask == endingMask)
+                        return 1+steps;
+                    if(!seen[ng][nextMask]) {
+                        seen[ng][nextMask] = true;
+                        nextQ.add(new int[]{ng, nextMask});
+                    }
+                }
             }
+            steps++;
+            q = nextQ;
         }
-        return cache[i][mask];
+        return -1;
     }
 }
