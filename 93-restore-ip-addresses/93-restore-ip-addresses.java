59d0fa8677ce384c2cfdc84abf8d80c340407528
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-03-03 22:40:44 +0530

    Time: 9 ms (40.37%), Space: 44.2 MB (7.75%) - LeetHub

--- 93-restore-ip-addresses/93-restore-ip-addresses.java
+++ 93-restore-ip-addresses/93-restore-ip-addresses.java
@@ -1,28 +1,23 @@
 class Solution {
     public List<String> restoreIpAddresses(String s) {
         List<String> ans = new ArrayList<>();
-        helper(s, ans, 0, 0, "");
+        helper(s, ans, 0, "");
         return ans;
     }
     
-    private void helper(String s, List<String> ans, int idx, int n, String curr) {
-        if(idx == s.length() || n == 4) {
-            if(idx == s.length() && n == 4) 
+    private void helper(String s, List<String> ans, int n, String curr) {
+        if(n == 4) {
+            if(s.length() == 0)
                 ans.add(curr.substring(0, curr.length()-1));
             return;
         }
-        
-        helper(s, ans, idx+1, n+1, curr+s.charAt(idx)+".");
-        if(idx+2 <= s.length() && isValid(s.substring(idx, idx+2)))
-            helper(s, ans, idx+2, n+1, curr+s.substring(idx, idx+2)+".");
-        if(idx+3 <= s.length() && isValid(s.substring(idx, idx+3)))
-            helper(s, ans, idx+3, n+1, curr+s.substring(idx, idx+3)+".");
-    }
-    
-    private boolean isValid(String str) {
-        if(str.charAt(0) == '0')
-            return false;
-        
-        return Integer.parseInt(str) <= 255;
+        for(int i=1; i<=3; i++) {
+            if(s.length() < i) continue;
+            int val = Integer.parseInt(s.substring(0, i));
+            // if(val > 255 || i != val+"".length())
+            //     continue;
+            if(val>255 || i!=String.valueOf(val).length()) continue;
+            helper(s.substring(i), ans, n+1, curr+s.substring(0, i)+".");
+        }
     }
 }
