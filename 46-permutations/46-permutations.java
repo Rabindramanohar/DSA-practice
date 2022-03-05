62ac421549bf0602cf078e8af1094aedce6f4e33
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-03-05 10:12:11 +0530

    Time: 2 ms (74.61%), Space: 45.1 MB (13.98%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,24 +1,29 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        helper(nums, ans, new boolean[nums.length], new ArrayList<>());
+        generate(0, nums, ans);
         return ans;
     }
     
-    private void helper(int[] arr, List<List<Integer>> ans, boolean[] vis, List<Integer> curr) {
-        if(curr.size() == arr.length) {
+    private void generate(int idx, int[] arr, List<List<Integer>> ans) {
+        if(idx == arr.length) {
+            List<Integer> curr = new ArrayList<>();
+            for(int i=0; i<arr.length; i++)
+                curr.add(arr[i]);
             ans.add(new ArrayList<>(curr));
             return;
         }
         
-        for(int i=0; i<arr.length; i++) {
-            if(!vis[i]) { // avoid adding one elements multiple times
-                curr.add(arr[i]);
-                vis[i] = true;
-                helper(arr, ans, vis, curr); // recursively add arr element to curr ds
-                curr.remove(curr.size()-1); // backtracking so need to remove the ele from curr ds and unmark the vis 
-                vis[i] = false;
-            }
+        for(int i=idx; i<arr.length; i++) {
+            swap(i, idx, arr);
+            generate(idx+1, arr, ans);
+            swap(i, idx, arr); // reswapping for backtracking
         }
+    }
+    
+    private void swap(int i, int j, int[] arr) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 }
