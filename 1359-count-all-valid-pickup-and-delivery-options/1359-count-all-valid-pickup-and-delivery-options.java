7da215438c902c30727d525382e130aa8262ac5c
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-03-06 21:44:30 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (27.57%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countOrders(int n) {
+        long ans = 1;
+        int MOD = 1000000007;
+        for(int i=1; i<=n; i++) {
+            ans *= i;
+            ans = ans*(2*i-1);
+            ans %= MOD;
+        }
+        return (int)ans;
+    }
+}
