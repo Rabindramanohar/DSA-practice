fa299dd9429b1e26a39b9f9640241d934d869eb5
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-03-08 21:51:20 +0530

    Time: 1 ms (46.18%), Space: 46.1 MB (24.90%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -11,23 +11,19 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        
-        if(head == null || head.next == null) {
+        if(head == null || head.next == null)
             return false;
-        }
         
         ListNode slow = head;
         ListNode fast = head;
         
         while(fast.next != null && fast.next.next != null) {
-            fast = fast.next.next;
             slow = slow.next;
+            fast = fast.next.next;
             
             if(slow == fast)
                 return true;
-            
         }
-        
         return false;
     }
 }
