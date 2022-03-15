2de2ae4a64798a14ef63274e9c2d53bc6e3bc8df
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-03-15 22:52:13 +0530

    Time: 38 ms (43.26%), Space: 55.1 MB (7.01%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,31 +1,22 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Stack<Integer> st = new Stack<>();
         StringBuilder sb = new StringBuilder(s);
+        Stack<Integer> st = new Stack<>();
         for(int i=0; i<sb.length(); i++) {
             char c = sb.charAt(i);
-            if(!st.isEmpty() && c == ')')
-                st.pop();
-            else if(st.isEmpty() && c == ')')
-                sb.setCharAt(i, '*');
-            
             if(c == '(')
                 st.push(i);
-        }
-        
-        if(!st.isEmpty()) {
-            while(st.size() > 0) {
-                int idx = st.pop();
-                sb.setCharAt(idx, '*');
+            if(c == ')') {
+                if(!st.isEmpty())
+                    st.pop();
+                else
+                    sb.setCharAt(i, '*');
             }
         }
-        StringBuilder ans = new StringBuilder();
-        for(int i=0; i<sb.length(); i++) {
-            char c = sb.charAt(i);
-            if(c != '*')
-                ans.append(c);
-        }
         
-        return ans.toString();
+        while(!st.isEmpty())
+            sb.setCharAt(st.pop(), '*');
+        
+        return sb.toString().replaceAll("\\*", ""); 
     }
 }
