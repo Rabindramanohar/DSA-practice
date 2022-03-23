925fc737b0ffffaaa9253793e8b2ea75e015bcfa
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-03-23 14:15:44 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (37.77%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,11 +1,21 @@
 class Solution {
+    // recursive
     public int uniquePaths(int m, int n) {
-        int permu = n+m-2;
-        int r = m-1;
-        double ans = 1;
-        for(int i=1; i<=r; i++) {
-            ans = (ans*(permu-r+i)/i);
+        int[][] dp = new int[m][n];
+        // for(int[] row: dp)
+        //     Arrays.fill(row, -1);
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++)
+                dp[i][j] = -1;
         }
-        return (int)ans;
+        return helper(0, 0, m, n, dp);
+    }
+    public int helper(int i, int j, int n, int m, int[][] dp) {
+        
+        if(i >= n || j >= m) return 0;
+        if(i == (n-1) && j == (m-1)) return 1;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = helper(i+1, j, n, m, dp) + helper(i, j+1, n, m, dp);
     }
 }
