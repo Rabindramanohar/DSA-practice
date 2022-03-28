54ed3d75733250518f9460990ca043eaeea25765
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-03-28 12:54:27 +0530

    Time: 1 ms (81.31%), Space: 43.6 MB (65.32%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,15 +1,23 @@
 class Solution {
-    public boolean search(int[] nums, int target) {
-        Arrays.sort(nums);
+    public boolean search(int[] nums, int k) {
         int l = 0, h = nums.length-1;
         while(l <= h) {
-            int mid = (l+h)/2;
-            if(nums[mid] == target)
+            int mid = l + (h-l)/2;
+            if(nums[mid] == k)
                 return true;
-            if(nums[mid] < target)
-                l = mid+1;
-            else
-                h = mid-1;
+            // when 1st half is sorted
+            if(nums[l] < nums[mid]) {
+                if(k < nums[l] || k > nums[mid])
+                    l = mid+1;
+                else
+                    h = mid-1;
+            } else if(nums[l] > nums[mid]) { // means search part lies in the range of both the sorted parts
+                if(k < nums[mid] || k > nums[h])
+                    h = mid-1;
+                else 
+                    l = mid+1;
+            } else
+                l++;
         }
         return false;
     }
