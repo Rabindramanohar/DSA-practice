895feb95e6be636daffa3c1c53a8b031dacb6f16
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-04-06 17:40:55 +0530

    Time: 1 ms (100.00%), Space: 41.4 MB (98.39%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
@@ -1,19 +1,24 @@
 class Solution {
-    // brute force T=O(n*n)
+    // with formula T = O(n+100*100)
     public int threeSumMulti(int[] arr, int target) {
-        int MOD = (int)(1e9+7), n = arr.length;
+        int MOD = (int)(1e9+7);
         long ans = 0;
-        for(int i=0; i<n; i++) {
-            int[] cnt = new int[101];
-            for(int j=i+1; j<n; j++) {
-                int k = target-arr[i]-arr[j];
-                if(k>=0 && k<=100 && cnt[k]>0) {
-                    ans += cnt[k];
-                    ans %= MOD;
-                }
-                cnt[arr[j]]++;
+        long[] cnt = new long[101];
+        for(int i: arr)
+            cnt[i]++;
+        
+        for(int i=0; i<=100; i++) {
+            for(int j=i; j<=100; j++) {
+                int k=target-i-j;
+                if(k<0 || k>100) continue;
+                if(i == j && j == k)
+                    ans += (cnt[i]*(cnt[i]-1)*(cnt[i]-2)/6);
+                else if(i == j && j != k)
+                    ans += ((cnt[i]*(cnt[i]-1)/2)*cnt[k]);
+                else if(i<j && j<k)
+                    ans += cnt[i]*cnt[j]*cnt[k];
             }
         }
-        return (int)ans;
+        return (int)(ans%MOD);
     }
 }
