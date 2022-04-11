ccfecb96f3a3161365d119675d1ef81395345259
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-04-11 18:25:45 +0530

    Time: 16 ms (20.93%), Space: 54.4 MB (70.15%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,14 +1,23 @@
 class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         int n = grid.length, m = grid[0].length;
-        int start = n*m - k % (n*m);
-        LinkedList<List<Integer>> ans = new LinkedList<>();
-        for(int i=start; i<n*m + start; i++) {
-            int j = i%(n*m), r = j/m, c = j%m;
-            if((i-start)%m == 0)
-                ans.add(new ArrayList<>());
-            ans.peekLast().add(grid[r][c]);
-        }
+        k %= (n*m);
+        reverse(grid, 0, n*m-1);
+        reverse(grid, 0, k-1);
+        reverse(grid, k, n*m-1);
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int[] row : grid)
+            ans.add(Arrays.stream(row).boxed().collect(Collectors.toList()));
         return ans;
+    }
+    private void reverse(int[][] arr, int l, int h) {
+        int n = arr.length, m = arr[0].length;
+        while(l<h) {
+            int r = l/m, c = l%m, i = h/m, j = h%m;
+            int tmp = arr[r][c];
+            arr[r][c] = arr[i][j];
+            arr[i][j] = tmp;
+            l++; h--;
+        }
     }
 }
