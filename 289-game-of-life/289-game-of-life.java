881f3af7547fb0add8ba10a55011a4a8df1e46fd
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-04-12 09:44:27 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (31.67%) - LeetHub

--- 289-game-of-life/289-game-of-life.java
+++ 289-game-of-life/289-game-of-life.java
@@ -1,12 +1,8 @@
 class Solution {
+    // inplace
     public void gameOfLife(int[][] board) {
         int r = board.length, c = board[0].length;
-        int[][] copyB = new int[r][c];
-        for(int i=0; i<r; i++) {
-            for(int j=0; j<c; j++) {
-                copyB[i][j] = board[i][j];
-            }
-        }
+        
         int[] dx = {1, 1, 0, -1, -1, -1, 0, 1};
         int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
         
@@ -14,13 +10,18 @@ class Solution {
             for(int j=0; j<c; j++) {
                 int alive = 0;
                 for(int k=0; k<8; k++) {
-                    if(isValid(i+dx[k], j+dy[k], r, c) && copyB[i + dx[k]][j+dy[k]] == 1)
+                    if(isValid(i+dx[k], j+dy[k], r, c) && Math.abs(board[i + dx[k]][j+dy[k]]) == 1)
                         alive++;
                 }
-                if(copyB[i][j] == 0 && alive == 3)
-                    board[i][j] = 1;
+                if(board[i][j] == 0 && alive == 3)
+                    board[i][j] = 2;
                 if(board[i][j] == 1 && (alive < 2 || alive > 3))
-                    board[i][j] = 0;
+                    board[i][j] = -1;
+            }
+        }
+        for(int i=0; i<r; i++) {
+            for(int j=0; j<c; j++) {
+                board[i][j] = board[i][j] > 0 ? 1 : 0;
             }
         }
     }
