ca81e2517793a02fc9da8f3b58e440df6ede5a24
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-04-14 14:53:58 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (88.85%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
@@ -12,13 +12,12 @@
  *         this.right = right;
  *     }
  * }
- **/
+ */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if (root == null) return null;
+        if(root == null) return null;
         
-        if(root.val == val)
-            return root;
-        return val<root.val ? searchBST(root.left, val) : searchBST(root.right, val);
+        if(val == root.val) return root;
+        return val < root.val ? searchBST(root.left, val) : searchBST(root.right, val);
     }
 }
