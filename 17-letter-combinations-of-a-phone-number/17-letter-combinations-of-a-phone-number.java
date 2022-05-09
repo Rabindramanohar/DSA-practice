492be41e998a1998fb6ff31a843cac2b30730ed6
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-05-09 15:14:58 +0530

    Time: 5 ms (42.13%), Space: 41.1 MB (85.46%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,10 +1,7 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
         List<String> ans = new ArrayList<>();
-        if(digits.length() == 0 || digits == null)
-            return ans;
-        String[] mapping = {
-            "0",
+        String[] map = {"0",
             "1",
             "abc",
             "def",
@@ -13,21 +10,20 @@ class Solution {
             "mno",
             "pqrs",
             "tuv",
-            "wxyz"
-        };
-        combination(digits, ans, mapping, "", 0);
+            "wxyz"};
+        if(digits.length() == 0) return ans;
+        combinations(digits, ans, map, "", 0);
         return ans;
     }
-    
-    private static void combination(String digits, List<String> ans, String[] mapping, String curr, int idx) {
+    private void combinations(String digits, List<String> ans, String[] map, String curr, int idx) {
+        // base case
         if(idx == digits.length()) {
             ans.add(curr);
             return;
         }
         
-        String letters = mapping[digits.charAt(idx) - '0'];
-        for(int i=0; i<letters.length(); i++) {
-            combination(digits, ans, mapping, curr + letters.charAt(i), idx+1);
-        }
+        String letter = map[digits.charAt(idx)-'0'];
+        for(int i=0; i<letter.length(); i++)
+            combinations(digits, ans, map, curr+letter.charAt(i), idx+1);
     }
 }
