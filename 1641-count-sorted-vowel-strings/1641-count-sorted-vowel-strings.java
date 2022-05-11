3a2b17525a13452562c8095f406c2812d8f91ec4
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-05-11 12:05:00 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (64.76%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -1,21 +1,6 @@
 class Solution {
-    // recursive T = O(5^n)
-    char[] ch = new char[]{'a', 'e', 'i', 'o', 'u'};
+    // combination with repetitions T = O(n)
     public int countVowelStrings(int n) {
-        int ans = 0;
-        for(char c : ch)
-            ans += helper(n-1, c);
-        return ans;
-    }
-    
-    private int helper(int n, char lCh) {
-        // base case
-        if(n == 0)
-            return 1;
-        int tmp = 0;
-        for(char c : ch)
-            if(lCh >= c)
-                tmp += helper(n-1, c);
-        return tmp;
+        return (n+4)*(n+3)*(n+2)*(n+1) / 24;
     }
 }
