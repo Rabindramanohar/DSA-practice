5c1c37ef3013aff8fddddd1d85c0823a24ccf34b
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-05-20 21:00:58 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (81.50%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    // recursion
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length, m = obstacleGrid[0].length;
+        int[][] dp = new int[n][m];
+        for(int[] row : dp)
+            Arrays.fill(row, -1);
+        return helper(n-1, m-1, obstacleGrid, dp);
+    }
+    private int helper(int i, int j, int[][] grid, int[][] dp) {
+        // base cases:
+        // System.out.println(grid[i][j]);
+        if(i>=0 && j>=0 && grid[i][j] == 1) return 0;
+        if(i<0 || j<0) return 0;
+        if(i==0 && j==0) return 1;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        int up = helper(i-1, j, grid, dp);
+        int left = helper(i, j-1, grid, dp);
+        return dp[i][j] = up+left;
+    }
+}
