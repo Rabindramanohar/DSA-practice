c57f3db853fa549c3ebd695df5086a477209a02c
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-05-28 23:15:16 +0530

    Time: 0 ms (100.00%), Space: 51.1 MB (48.77%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,10 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int cnt = 0;
-        int n = nums.length;
-        for(int i=0; i<nums.length; i++) 
-            cnt += nums[i];
-        int total = n*(n+1)/2;
-        return total-cnt;
+        int xorArr=0;
+        for(int ele : nums)
+            xorArr ^= ele;
+        int xorAll=0;
+        for(int i=0; i<=nums.length; i++)
+            xorAll ^= i;
+        
+        return xorAll^xorArr;
     }
 }
