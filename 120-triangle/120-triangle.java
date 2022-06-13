dd7c49dd143ff5fdb3070329a968f4755133e249
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-06-13 23:25:21 +0530

    Update 120-triangle.java

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,16 +1,14 @@
 class Solution {
+    // memoization
     Integer[][] dp;
     public int minimumTotal(List<List<Integer>> triangle) {
         dp = new Integer[triangle.size()][triangle.size()];
         return helper(triangle, 0, 0);
     }
     
-    // Map<String, Integer> hm = new HashMap<>();
-    
     private int helper(List<List<Integer>> list, int i, int j) {
         if(i >= list.size()) return 0;
         
-        // String key = i + "|" + j;
         if(dp[i][j] == null) {
             dp[i][j] = Math.min(helper(list, i+1, j), helper(list, i+1, j+1)) + list.get(i).get(j);
         }
