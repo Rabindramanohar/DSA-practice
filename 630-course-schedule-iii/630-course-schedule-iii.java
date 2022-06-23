cce25e5002a93b52efdfb164b0234175d4f993dd
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-06-24 00:57:49 +0530

    Time: 75 ms (23.33%), Space: 69 MB (61.85%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.java
+++ 630-course-schedule-iii/630-course-schedule-iii.java
@@ -1,14 +1,14 @@
 class Solution {
     public int scheduleCourse(int[][] courses) {
-        Arrays.sort(courses,(a,b)->a[1]-b[1]); //Sort the courses by their deadlines (Greedy! We have to deal with courses with early deadlines first)
-        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
+        Arrays.sort(courses, (a, b) -> a[1]-b[1]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b-a);
         int time=0;
-        for (int[] c:courses) 
-        {
-            time+=c[0]; // add current course to a priority queue
-            pq.add(c[0]);
-            if (time>c[1]) time-=pq.poll(); //If time exceeds, drop the previous course which costs the most time. (That must be the best choice!)
-        }        
+        for(int[] c : courses) {
+            time += c[0];
+            pq.offer(c[0]);
+            if(time > c[1])
+                time -= pq.poll();
+        }
         return pq.size();
     }
 }
