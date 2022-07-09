b139ecf4421fdc9bfe9193cd2126ecc67e1ce4b7
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-07-09 23:49:47 +0530

    Time: 58 ms (49.58%), Space: 84.2 MB (77.07%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.java
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -1,17 +1,18 @@
 class Solution {
     public int maxResult(int[] nums, int k) {
-        // priority queue
-        int n = nums.length, max=nums[0];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1]-a[1]);
-        pq.offer(new int[]{0, nums[0]});
+        // deque
+        int n = nums.length;
+        Deque<Integer> dq = new ArrayDeque<>();
+        dq.offerLast(0);
         for(int i=1; i<n; i++) {
-            while(i-pq.peek()[0] > k) {
-                pq.poll();
-            }
-            int[] top = pq.peek();
-            max = nums[i] + top[1];
-            pq.offer(new int[]{i, max});
+            nums[i] = nums[i] + nums[dq.peekFirst()];
+            while(!dq.isEmpty() && nums[i] >= nums[dq.peekLast()])
+                dq.pollLast();
+            dq.offerLast(i);
+            
+            if(i-dq.peekFirst() >= k)
+                dq.pollFirst();
         }
-        return max;
+        return nums[n-1];
     }
 }
