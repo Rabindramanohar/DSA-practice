c0195d8ee04dc169f2187419238995d655f76075
Rabindra Kumar Mahato <robinmanohar@gmail.com>
2022-07-18 22:05:53 +0530

    Time: 267 ms (47.89%), Space: 117.2 MB (58.81%) - LeetHub

---
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numSubmatrixSumTarget(int[][] matrix, int target) {
+        int n = matrix.length, m = matrix[0].length;
+        
+        for(int i=0; i<n; i++) {
+            for(int j=1; j<m; j++) {
+                matrix[i][j] += matrix[i][j-1];
+            }
+        }
+        
+        int cnt = 0;
+        for(int c1=0; c1<m; c1++) {
+            for(int c2=c1; c2<m; c2++) {
+                Map<Integer, Integer> hm = new HashMap<>();
+                hm.put(0, 1);
+                int sum = 0;
+                
+                for(int r=0; r<n; r++) {
+                    sum += matrix[r][c2] - (c1 > 0 ? matrix[r][c1-1] : 0);
+                    cnt += hm.getOrDefault(sum-target, 0);
+                    hm.put(sum, hm.getOrDefault(sum, 0)+1);
+                }
+            }
+        }
+        
+        return cnt;
+    }
+}
